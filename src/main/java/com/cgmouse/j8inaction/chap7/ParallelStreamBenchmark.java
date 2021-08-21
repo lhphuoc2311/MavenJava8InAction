9501916038c952b8aff6b5ad44c4d782b0f5d585
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-21 14:31:42 +0700

    added measuring performance of a funciton

---
+++ src/main/java/com/cgmouse/j8inaction/chap7/ParallelStreamBenchmark.java
@@ -0,0 +1,35 @@
+package com.cgmouse.j8inaction.chap7;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.TearDown;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Fork(value = 2, jvmArgs ={"-Xms4G", "-Xmx4G"})
+public class ParallelStreamBenchmark {
+
+    private static final long N = 10_000_000L;
+
+    //using internal iterative stream
+    @Benchmark
+    public static long sequentialSum() {
+        return Stream.iterate(1L, num -> num + 1)
+        .limit(N)
+        .reduce(0L, Long::sum);
+    }
+
+    @TearDown(Level.Invocation)
+    public void tearDown(){
+        System.gc();
+    }
+
+    
+}
