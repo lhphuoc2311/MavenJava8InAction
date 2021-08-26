f2bdf3edba606312d76ac541db1e5e985e1d7420
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-26 20:12:33 +0700

    updated using Anonymous class

--- src/main/java/com/cgmouse/j8inaction/chap2/GUIEventHandling.java
+++ src/main/java/com/cgmouse/j8inaction/chap2/GUIEventHandling.java
@@ -1,7 +1,26 @@
 package com.cgmouse.j8inaction.chap2;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
 public class GUIEventHandling {
     public static void main(String[] args) {
+
+        //Using anonymous class
+        Button button = new Button("Send");
+
+        button.setOnAction(new EventHandler<ActionEvent>(){
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World");
+            }
+            
+        });
+
+       
+
         
     }
     
