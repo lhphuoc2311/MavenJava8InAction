fe9b255c446c2262f79c3bdb46709d8518dcab97
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-26 17:17:16 +0700

    updated using anonymous class

--- src/main/java/com/cgmouse/j8inaction/chap2/ExecutingBlockCodeWithRunnable.java
+++ src/main/java/com/cgmouse/j8inaction/chap2/ExecutingBlockCodeWithRunnable.java
@@ -2,9 +2,20 @@ package com.cgmouse.j8inaction.chap2;
 
 public class ExecutingBlockCodeWithRunnable {
 
-    //using anonymous class containing a run method
+    
 
     public static void main(String[] args) {
+
+        //using anonymous class containing a run method
+        Thread t = new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                System.out.println("Hello World");                
+            }
+            
+        });
+        t.run();
         
     }
 
