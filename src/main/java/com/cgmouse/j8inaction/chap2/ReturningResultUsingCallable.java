767dfde2ad2542b485304431d9e09fdb20073c47
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-26 19:25:19 +0700

    updated using ExecuterService interface and funtional interface Callable

--- src/main/java/com/cgmouse/j8inaction/chap2/ReturningResultUsingCallable.java
+++ src/main/java/com/cgmouse/j8inaction/chap2/ReturningResultUsingCallable.java
@@ -1,7 +1,28 @@
 package com.cgmouse.j8inaction.chap2;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ReturningResultUsingCallable {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+       Future<String> future =  executorService.submit(new Callable<String>(){
+
+            @Override
+            public String call() throws Exception {
+                return Thread.currentThread().getName();
+            }
+            
+        });
+
+        executorService.shutdown();
+
+        System.out.println(future.get());
         
     }
     
