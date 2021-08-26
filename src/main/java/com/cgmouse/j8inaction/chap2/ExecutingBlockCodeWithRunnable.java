19514061f9b16a536fe8cc18a7198fa7b0be7d1c
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-26 17:14:21 +0700

    added  initial ExecutingBlockCodeWithRunnable.java

---
+++ src/main/java/com/cgmouse/j8inaction/chap2/ExecutingBlockCodeWithRunnable.java
@@ -0,0 +1,12 @@
+package com.cgmouse.j8inaction.chap2;
+
+public class ExecutingBlockCodeWithRunnable {
+
+    //using anonymous class containing a run method
+
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
