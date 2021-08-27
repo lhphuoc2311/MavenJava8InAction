a48371efe7585fe0d5397cb7544c905b51e50970
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-27 23:25:55 +0700

    updated reading one line from a file

--- src/main/java/com/cgmouse/j8inaction/chap2/PuttingLambdasIntoPractice.java
+++ src/main/java/com/cgmouse/j8inaction/chap2/PuttingLambdasIntoPractice.java
@@ -1,9 +1,23 @@
 package com.cgmouse.j8inaction.chap2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PuttingLambdasIntoPractice {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(processFile());
         
     }
-    
+
+    public static String processFile() throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+           
+            return br.readLine();
+
+        }
+    }
+
 }
